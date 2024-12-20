3f3a19133c9684353a8fcbb5c5836d258d3fe381
asher967 <asher.chan555@gmail.com>
2024-12-20 17:38:10 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+// Create the iframe element
+const iframe = document.createElement("iframe");
+
+// Set the iframe's source URL
+iframe.src = "https://unblocked-games.s3.amazonaws.com/index.html";  
+
+// Append the iframe to the body of the document
+document.body.appendChild(iframe);
